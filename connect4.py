a2b911530d97e4b06a377738e91d49c7a8f98d2c
ammbience <105063908+ammbience@users.noreply.github.com>
2023-09-08 09:25:40 -0500

    fuck this program

--- connect4.py
+++ connect4.py
@@ -7,39 +7,10 @@ grid = [[".", ".", ".", ".", ".", ".", "."], # 0
         [".", ".", ".", ".", ".", ".", "."], # 3
         [".", ".", ".", ".", ".", ".", "."], # 4
         [".", ".", ".", ".", ".", ".", "."]] # 5
-grid_checklist = []
-
-
-# updates the list used to check for all possible winning scenarios
-def update_checklist():
-    global grid_checklist           # vertical
-    grid_checklist = [[grid[0][0], grid[1][0], grid[2][0], grid[3][0], grid[4][0], grid[5][0]],
-                    [grid[0][1], grid[1][1], grid[2][1], grid[3][1], grid[4][1], grid[5][1]],
-                    [grid[0][2], grid[1][2], grid[2][2], grid[3][2], grid[4][2], grid[5][2]],
-                    [grid[0][3], grid[1][3], grid[2][3], grid[3][3], grid[4][3], grid[5][3]],
-                    [grid[0][4], grid[1][4], grid[2][4], grid[3][4], grid[4][4], grid[5][4]],
-                    [grid[0][5], grid[1][5], grid[2][5], grid[3][5], grid[4][5], grid[5][5]],
-                    [grid[0][6], grid[1][6], grid[2][6], grid[3][6], grid[4][6], grid[5][6]],
-                    # down, right
-                    [grid[0][3], grid[1][4], grid[2][5], grid[3][6]],
-                    [grid[0][2], grid[1][3], grid[2][4], grid[3][5], grid[4][6]],
-                    [grid[0][1], grid[1][2], grid[2][3], grid[3][4], grid[4][5], grid[5][6]],
-                    [grid[0][0], grid[1][1], grid[2][2], grid[3][3], grid[4][4], grid[5][5]],
-                    [grid[1][0], grid[2][1], grid[3][2], grid[4][3], grid[5][4]],
-                    [grid[2][0], grid[3][1], grid[4][2], grid[5][3]],
-                    # down, left / up, right
-                    [grid[3][0], grid[2][1], grid[1][2], grid[0][3]],
-                    [grid[4][0], grid[3][1], grid[2][2], grid[1][3], grid[0][4]],
-                    [grid[5][0], grid[4][1], grid[3][2], grid[2][3], grid[1][4], grid[0][5]],
-                    [grid[5][1], grid[4][2], grid[3][3], grid[2][4], grid[1][5], grid[0][6]],
-                    [grid[5][2], grid[4][3], grid[3][4], grid[2][5], grid[1][6]],
-                    [grid[5][3], grid[4][4], grid[3][5], grid[2][6]]]
 
 
 # runs the actual game
-def main():
-    global grid
-    global turn
+def main(grid, turn):
     while True:
         try:
             print_grid()
@@ -70,26 +41,6 @@ def main():
 # checks for every possible win scenario
 def win_checker(turn):
     global grid
-    global grid_checklist
-    for row in grid_checklist:
-        counter = 0
-        while counter <= len(row) - 4:
-            checklist = [row[counter], row[counter + 1], row[counter + 2], row[counter + 3]]
-            if checklist == [turn, turn, turn, turn]:
-                return True
-            else:
-                counter += 1
-                continue
-    for row in grid:
-        counter = 0
-        while counter <= len(row) - 4:
-            checklist = [row[counter], row[counter + 1], row[counter + 2], row[counter + 3]]
-            if checklist == [turn, turn, turn, turn]:
-                return True
-            else:
-                counter += 1
-                continue
-    return False
 
 
 # displays the connect 4 board/grid
@@ -111,10 +62,10 @@ def update_board(move):
     while counter >= 0:
         if grid[counter][column] == ".":
             grid[counter][column] = turn
-            update_checklist()
             break
         else:
             counter -= 1
+
 
 os.system("cls")
 print("Welcome To Connect 4!")
@@ -123,4 +74,4 @@ print("To Start, Press Enter.")
 start = input()
 
 
-main()
+main(grid, turn)
